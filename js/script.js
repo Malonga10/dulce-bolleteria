4d404a0d25d208c45277d861dab97e75bf2254b3
Malonga10 <irving200516@gmail.com>
2026-07-30 22:25:24 -0500

    Fix image path for 'Plátano, Café Espresso y Dátil'
    Updated image path for 'Plátano, Café Espresso y Dátil' panqué.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const products = [
   {id:'naranja', name:'Panqué de Naranja', cats:['clasicos'], desc:'Clásico de la casa con un fresco sabor a naranja natural.',  price:270, img:'imgs/panque_naranja.jpeg'},
   {id:'platano-nuez', name:'Panqué de Plátano con Nuez', cats:['clasicos'], desc:'Plátanos maduros, dulzor natural y textura suave y esponjosa.', price:270, img:'imgs/panque_platano_nuez.jpeg'},
   {id:'datil', name:'Panqué de Dátil', cats:['saludables'], desc:'Harina de avena, dátiles maduros y dulzor natural.', price:300, img:'imgs/panque_datil.jpeg'},
-  {id:'platano-cafe-datil', name:'Plátano, Café Espresso y Dátil', cats:['clasicos', 'saludables'], desc:'Panqué artesanal elaborado con platanos maduros, café espresso y dátiles.',  price:320, img:'imgs/panque_platano.jpeg'},
+  {id:'platano-cafe-datil', name:'Plátano, Café Espresso y Dátil', cats:['clasicos', 'saludables'], desc:'Panqué artesanal elaborado con platanos maduros, café espresso y dátiles.',  price:320, img:'imgs/panque_datil2.jpeg'},
   {id:'zanahoria', name:'Panqué de Zanahoria', cats:['clasicos'], desc:'Panqué clásico de zanahoria, húmedo y lleno de sabor.', price:270, img:'imgs/panque_zanahoria.jpeg'},
   {id:'zanahoria-coco', name:'Panqué de Zanahoria con Coco', cats:['clasicos'], desc:'Nuestra zanahoria clásica con un toque tropical de coco tostado.', price:290, img:'imgs/panque_zana_coco.jpeg'},
   {id:'chocolate', name:'Panqué de Chocolate', cats:['clasicos'], desc:'Suave, húmedo y esponjoso, con un delicioso sabor a chocolate.', price:270, img:'imgs/panque_chocolate_normal.jpeg'},
